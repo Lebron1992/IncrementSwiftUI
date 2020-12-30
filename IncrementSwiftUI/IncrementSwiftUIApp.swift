6dc4a899cb6aa04ca85580b50cd978b1885655be
Lebron1992 <wenzhi.zeng@outlook.com>
2020-12-30 16:25:34 +0800

    Initial Commit

---
+++ IncrementSwiftUI/IncrementSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncrementSwiftUIApp.swift
+//  IncrementSwiftUI
+//
+//  Created by Lebron on 2020/12/30.
+//
+
+import SwiftUI
+
+@main
+struct IncrementSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
